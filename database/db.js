059325bd9a744fbbcc5f34bf6c1a0e3413c4921b
srijit0x <163331906+srijit0x@users.noreply.github.com>
2024-11-14 00:52:09 +0100

    update error catching

--- database/db.js
+++ database/db.js
@@ -3,11 +3,47 @@ require('dotenv').config();
 
 const mongoDBConnectionString = process.env.MONGODB_URI;
 
-mongoose.connect(mongoDBConnectionString, {
+const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connection established successfully'))
-.catch((error) => console.error('Failed to connect to MongoDB:', error));
+  autoIndex: false,
+  maxPoolSize: 10,
+  serverSelectionTimeoutMS: 5000,
+  socketTimeoutMS: 45000,
+  family: 4
+};
+
+const connectWithRetry = () => {
+  console.log('Attempting to connect to MongoDB...');
+  mongoose.connect(mongoDBConnectionString, options)
+    .then(() => console.log('MongoDB connection established successfully'))
+    .catch((error) => {
+      console.error('Failed to connect to MongoDB:', error.message);
+      
+      console.log('Retrying MongoDB connection in 5 seconds...');
+      setTimeout(connectWithRetry, 5000);
+    });
+};
+
+connectWithRetry();
+
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose connected to DB.');
+});
+
+mongoose.connection.on('error', (error) => {
+    console.log('Mongoose connection error:', error);
+});
+
+mongoose.connection.on('disconnected', () => {
+    console.log('Mongoose disconnected from DB.');
+});
+
+process.on('SIGINT', () => {
+    mongoose.connection.close(() => {
+        console.log('Mongoose connection disconnected due to app termination.');
+        process.exit(0);
+    });
+});
 
 module.exports = mongoose.connection;

41db48024112eb251fbc1490e985e0f49bf2d464
srijit0x <163331906+srijit0x@users.noreply.github.com>
2024-12-23 18:43:56 +0100

    error catching update

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -2,10 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+class ErrorBoundary extends React.Component {
+  constructor(props: {}) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error: Error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+    console.error("Uncaught error:", error, errorInfo);
+  }
+
+  render() {
+    if ((this.state as any).hasError) {
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>
 );
